6aaf1550d3c4b4435ccf367d55934135db2e2447
zhangsean <zxf2342@qq.com>
2021-02-24 18:38:45 +0800

    Add date type json

--- generate/g_appcode.go
+++ generate/g_appcode.go
@@ -108,6 +108,7 @@ var typeMappingMysql = map[string]string{
 	"binary":    "string", // binary
 	"varbinary": "string",
 	"year":      "int16",
+	"json":      "string",
 }
 
 // typeMappingPostgres maps SQL data type to corresponding Go data type
